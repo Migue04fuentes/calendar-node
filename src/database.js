a7fe3a2c3ef8bdfbfcfe50e604e39d0738173e67
Miguel Ramírez <miguel03rf@gmail.com>
2022-11-03 16:11:15 -0500

    conexióndb gitignore

---
+++ src/database.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const { MONGODB_HOST, MONGODE_DATABASE} = process.env;
+const MONGODB_URI = `mongodb://${MONGODB_HOST}/${MONGODE_DATABASE}`;
+
+mongoose.connect(MONGODB_URI, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+})
+
+    .then(db => console.log('Database is connected'))
+    .catch(err => console.log(err));
--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
+
+require('dotenv').config();
+
 // Importación del server app
 const app = require('./server');
+require('./database');
 
 //Lo que va a realizar cuando se ejecute el puerte 
 app.listen(app.get('port'), () => {
     console.log('server on port:', app.get('port'));
 });
+
+
