3542227ad725a25ca402062ab342f08526203cf0
Miguel Ramírez <miguel03rf@gmail.com>
2022-11-04 11:53:13 -0500

    plantillas handlebars

--- src/models/agendas.js
+++ src/models/agendas.js
@@ -1,1 +1,18 @@
-const {} = require('mongoose');
+// Definicón del modelo de datos
+const {Schema, model} = require('mongoose');
+
+
+const agendaschema = new Schema({
+    title: {
+        type :String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    }
+},{
+ //   timestamps: true   //fecha de creación y actulización
+})  
+
+module.exports = model('Agenda', agendaschema);
--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 //Módulos
 //Llamando el framework del servidor
 const express = require('express');
+const exphbs = require('express-handlebars');
 const path = require('path');
+
+
 //Inicialización
 //servidor
 const app = express();
@@ -9,8 +12,13 @@ const app = express();
 //Configuraciones 
 app.set('port', process.env.PORT || 4000);
 app.set('views', path.join(__dirname, 'views'));
-
-
+app.engine('.hbs', exphbs.engine({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
 // Middlewares
 app.use(express.urlencoded({extended: false}));
 
@@ -20,7 +28,7 @@ app.use(express.urlencoded({extended: false}));
 
 //Rutas
 app.get('/', (req, res) => {
-    res.send('Calendar');
+    res.render('index');
 });
 
 
